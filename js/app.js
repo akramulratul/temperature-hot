24d956a100ecdc2d64c5ac571778b53e294b40d4
akramulratul <akramulratul@gmail.com>
2021-09-23 02:47:22 +0600

    add icon for weather change

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
-console.log("click");
-const app_key = "e8f2ccea0350442dc80663c8d667bf37";
+const api_key = "e8f2ccea0350442dc80663c8d667bf37";
 const searchTemperature = () => {
   const city = document.getElementById("city-name").value;
-  
-  const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${app_key}`;
-  const fetch('api').than(response=>response.json()).than(data=>console.log(data))
+  document.getElementById("city-name").value = "";
+
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+  fetch(api)
+    .then((res) => res.json())
+    .then((data) => displayTemperature(data));
+};
+
+const setInnerText = (id, text) => {
+  document.getElementById(id).innerText = text;
+};
+const displayTemperature = (temperature) => {
+  setInnerText("name", temperature.name);
+  setInnerText("temperature", temperature.main.temp);
+  setInnerText("weather", temperature.weather[0].main);
+  //set weather icon
+  const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+  const imgIcon = document.getElementById("weather-icon");
+  imgIcon.setAttribute("src", url);
 };
